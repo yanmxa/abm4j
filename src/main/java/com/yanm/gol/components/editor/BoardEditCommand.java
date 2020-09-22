315ec3097f671c042fe08399fbcffeaee91ddda8
Yanmeng <ymeng125@126.com>
2020-09-22 23:34:48 +0800

    add UndoableCommand with batch

--- src/main/java/com/yanm/gol/components/editor/BoardEditCommand.java
+++ src/main/java/com/yanm/gol/components/editor/BoardEditCommand.java
@@ -2,31 +2,37 @@ package com.yanm.gol.components.editor;
 
 import com.yanm.gol.model.Board;
 import com.yanm.gol.model.CellPosition;
-import com.yanm.gol.model.CellState;
 
 public class BoardEditCommand implements UndoableEditorCommand {
 
-    private CellPosition position;
-    private CellState drawMode;
-    private CellState prevState;
 
-    public BoardEditCommand(CellPosition position, CellState drawMode, CellState prevState) {
-        this.position = position;
-        this.drawMode = drawMode;
-        this.prevState = prevState;
+    private Edits edits;
+
+    public BoardEditCommand(Edits edits) {
+        this.edits = new Edits(edits);
     }
 
     @Override
     public void undo(EditorState editorState) {
         Board board = editorState.getEditorBoard().get();
-        board.setState(position.getX(), position.getY(), prevState);
+
+        for (Change edit : edits) {
+            CellPosition position = edit.getPostition();
+            board.setState(position.getX(), position.getY(), edit.getPrevState());
+        }
+
         editorState.getEditorBoard().set(board);
     }
 
     @Override
     public void execute(EditorState editorState) {
         Board board = editorState.getEditorBoard().get();
-        board.setState(position.getX(), position.getY(), drawMode);
+
+        for (Change edit : edits) {
+            CellPosition position = edit.getPostition();
+            board.setState(position.getX(), position.getY(), edit.getNewState());
+        }
+
         editorState.getEditorBoard().set(board);
     }
 }
